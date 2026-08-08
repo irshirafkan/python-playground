e55161d8102ab5159466c975db0bb9973995bb29
irshirafkan <irshirafkan@gmail.com>
2026-08-08 15:38:26 +0330

    discontinuous string exercise

---
+++ Data Structures in Python/discontinuous_string.py
@@ -0,0 +1,9 @@
+# Discontinuous String Exercise
+#
+# Write a code that first converts the following string to lowercase,
+# and then outputs the string as a list of separate words using a string method.
+
+
+text = input()
+result = text.lower().split()
+print(result)
