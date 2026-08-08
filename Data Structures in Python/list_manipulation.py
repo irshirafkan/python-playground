6859864a77165e24f1ffc5cee479e7cdedbd3548
irshirafkan <irshirafkan@gmail.com>
2026-08-08 15:50:31 +0330

    list manipulation exercise

---
+++ Data Structures in Python/list_manipulation.py
@@ -0,0 +1,10 @@
+# List Manipulation Exercise
+#
+# Write a code that uses list operators (such as removing and appending
+# elements to the end of the list) to remove the number 3 from the given list
+# and add the number 10 to the end of the list. Then return the final list.
+
+my_list = [1, 2, 3, 4, 5]
+my_list.remove(3)
+my_list.append(10)
+print(my_list)
